1077cf9419281f9f365928ee51dd7a3289525f10
zoman_hp <zomanhome@gmail.com>
2019-03-08 03:17:03 +0200

    Updates

---
+++ precache-manifest.4f4a4033924f0c05d2e6c46ade91f30c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "870c71cdd0467af692b4",
+    "url": "/simpleblog/static/js/runtime~main.dfe4ec47.js"
+  },
+  {
+    "revision": "d0160d238f4b77f52cf0",
+    "url": "/simpleblog/static/js/main.10160e61.chunk.js"
+  },
+  {
+    "revision": "964e2bae0ca73926739d",
+    "url": "/simpleblog/static/js/2.713034f0.chunk.js"
+  },
+  {
+    "revision": "d0160d238f4b77f52cf0",
+    "url": "/simpleblog/static/css/main.3691c6dd.chunk.css"
+  },
+  {
+    "revision": "7d3f79d72cf4dfcc8f3cfb75ae6759da",
+    "url": "/simpleblog/index.html"
+  }
+];
